b591a59da75566564fed434904f5258bac936d69
fabiopiro <piroddifabio01@gmail.com>
2021-05-12 16:11:17 +0200

    snack 1

--- Snack 1/js/script.js
+++ Snack 1/js/script.js
@@ -22,27 +22,24 @@ var bici = [
 ];
 console.log(bici);
 
-var [bici1,bici2,bici3] = bici;
-
-console.log(bici1.peso);
-console.log(bici2.peso);
-console.log(bici3.peso);
-
-
-
-var biciPeso = 99;
-var biciLeggera = "";
+var biciLeggera = {
+    nome: "",
+    peso: 99,
+};
 
 for (var i = 0; i < bici.length; i++) { 
 
-    if (bici[i].peso < biciPeso) {
-        biciPeso = bici[i].peso;
-        biciLeggera = bici[i].nome;
+    if (bici[i].peso < biciLeggera.peso) {
+        biciLeggera = bici[i];
     }
 
 }
+console.log(biciLeggera);
+
+var {nome} = biciLeggera;
+var {peso} = biciLeggera;
 
 document.getElementById("bici_leggera").innerHTML = `
-La bici più leggera è la ${biciLeggera} con un peso di ${biciPeso}
+La bici più leggera è la ${nome} con un peso di ${peso}
 `
 
